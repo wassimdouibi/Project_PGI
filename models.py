d0f00342a5b9407e40284cfa3d4070fc8c1d3441
Wassim <lw_douibi@esi.dz>
2025-01-03 00:11:38 +0100

    Refactor reclamation models for consistency and clarity in fields definitions

--- models.py
+++ models.py
@@ -1,134 +1,124 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 #  Reclamation / Equipe / Appel / Reclamant / Agent clientele(nom + reclamations)
 
+# ------------------------------ Réclamation ------------------------------
 class Reclamation(models.Model):
-    _inherit = 'crm.lead'  # Hérite de crm.lead
-    
-    _name = 'gestion_de_reclation.Reclamation'
-    # Description spécifique pour les réclamations
-    _description = "La liste des Réclamations"
+    # Hérite de crm.lead
+    _inherit = "crm.lead"
+    # Meta-données
+    _name = "gestion_de_reclamation.Reclamation"
+    _description = "La liste des Réclamations"  # Description spécifique pour les réclamations
 
-    # Champs personnalisés pour les réclamations
-    date_reclamation = fields.Date(string="Date de Réclamation", required=True)
-    
-    objet = fields.Char(string="Objet de la Réclamation", required=True)
-    
+    # Champs
+    date_reclamation = fields.Date(string="Date de Réclamation", required=True)  # date de creation de la reclamation
+    objet = fields.Char(string="Objet de la Réclamation", required=True)  #
     description = fields.Text(string="Description")
-     
+
     # Catégorisation des réclamations
     type_reclamation = fields.Selection(
-        [('technique', 'Technique'), ('commerciale', 'Commerciale')],
+        [("technique", "Technique"), ("commerciale", "Commerciale")],
         string="Type de Réclamation",
         required=True
     )
-    
+
     # Origine de la réclamation
     origine_reclamation = fields.Selection(
-        [('citoyen', 'Citoyen'), ('entreprise', 'Entreprise'), ('cellule_veille', 'Cellule de Veille')],
+        [("citoyen", "Citoyen"), ("entreprise", "Entreprise"), ("cellule_veille", "Cellule de Veille")],
         string="Origine de la Réclamation",
         required=True
     )
 
     # Informations sur le reclamant
     # Champs many2one vers reclamant 
-    #Champs many2one vers agentClientele(id user)
-    
-   # champs many2one vers equipe technique
+    # Champs many2one vers agentClientele(id user)
+
+    # champs many2one vers equipe technique
     equipe_technique_id = fields.Many2one(
-        'gestion_de_reclation.equipe_technique', 
-        string="Equipe Technique", 
+        "gestion_de_reclamation.equipe_technique",
+        string="Equipe Technique",
         required=True,
-        ondelete='restrict')
-    
-    
+        ondelete="restrict")
+
     equipe_commercial_id = fields.Many2one(
-        'gestion_de_reclation.equipe_commercial', 
-        string="Equipe Commerciale", 
+        "gestion_de_reclamation.equipe_commercial",
+        string="Equipe Commerciale",
         required=True,
-        ondelete='restrict')
+        ondelete="restrict")
 
     # Traitement
     urgente = fields.Boolean(string="Est Urgente ?", default=False)
-    
+
     etat = fields.Selection(
-        [('nouveau', 'Nouveau'), ('en_cours', 'En Cours'), ('resolu', 'Résolu'), ('archive', 'Archivé')],
+        [("nouveau", "Nouveau"), ("en_cours", "En Cours"), ("resolu", "Résolu"), ("archive", "Archivé")],
         string="État",
-        default='nouveau'
+        default="nouveau"
     )
 
     def generate_accuse(self):
         """Action pour générer un accusé de réclamation en PDF"""
-        return self.env.ref('gestion_de_reclamation.action_accuse_reclamation').report_action(self)      
-    
-    
-
-    
+        return self.env.ref("gestion_de_reclamation.action_accuse_reclamation").report_action(self)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ------------------------------ Appel ------------------------------
 class Appel(models.Model):
     # meta-données
-    _name = 'gestion_de_reclation.appel'
+    _name = "gestion_de_reclamation.appel"
     _description = "Appel modèle à des fins de réclamation"
-    #champs
+
+    # champs
+    label = fields.Char(
+        string="IdAppel",
+        required=True
+    )  # identifiant de l'appel
     objetAppel = fields.Char(
-        string = 'Objet de l\'appel',
-        required = True
-    ) # l'objet de l'appel en general
-    
+        string="Objet de l'appel",
+        required=True
+    )  # l"objet de l"appel en general
+    descriptionAppel = fields.Text(
+        string="Les points abordés dans l'appel",
+        required=False
+    )
     dateAppel = fields.Date(
         string="Date de l'appel"
-    ) # la date de creation de l'appel
+    )  # la date de creation de l"appel
 
-
-    
     nomAppelant = fields.Char(
-        string = 'Nom de l\'appelant',
-        required = True
-    ) # le nom de l'appelant
-    
+        string="Nom de l'appelant",
+        required=True
+    )  # le nom de l"appelant
     prenomAppelant = fields.Char(
-        string = 'Prenom de l\'appelant',
-        required = True
-    ) # le prenom de l'appelant
-    
+        string="Prenom de l'appelant",
+        required=True
+    )  # le prenom de l"appelant
     numeroAppelant = fields.Char(
-        string = 'Numero de l\'appelant',
-        required = True
-    ) # le numero de l'appelant
-    
-    
-    
+        string="Numero de l'appelant",
+        required=True
+    )  # le numero de l"appelant
 
+    idAgentClientele = fields.Many2one(
+        "gestion_de_reclamation.agent_clientele",
+        string="Agent clientele",
+        required= True,
+    )  # id de l"agent qui a répondu à l"appel
 
+    # ------------------------------ Equipe technique ------------------------------
     class EquipeTechnique(models.Model):
-        _inherit = 'crm.team'
-        
-        _name = 'gestion_de_reclation.equipe_technique'
+        _inherit = "crm.team"
+
+        _name = "gestion_de_reclamation.equipe_technique"
         _description = "Equipe technique qui gere une reclamation technique"
 
+    # ------------------------------ Commission de redressement ------------------------------
+    class CommissionDeRedressement(models.Model):
+        _inherit = "crm.team"
 
-
-
-    class EquipeCommercial(models.Model):
-        _inherit = 'crm.team'
-
-        _name = 'gestion_de_reclation.equipe_commerciale'
+        _name = "gestion_de_reclamation.commissionDeRedressement"
         _description = "Equipe commerciale qui gere une reclamation commerciale"
-    
-    
+
+    # ------------------------------ Agent Clientèle ------------------------------
+    class AgentClientele(models.Model):
+        _name = "gestion_de_reclamation.agent_clientele"
+        _description = "Agent clientèle en charge de la préoccupation des besoins de réclamation"
